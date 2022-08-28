2350e972adf7b38eb1336bba35b0bf7894e32e4d
Yusuke INAGAKI <7163746+diaphragm@users.noreply.github.com>
2022-08-29 04:46:59 +0900

    add cmd script to launch

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 cd stable-diffusion
-python scripts/webui.py
+
+if [ "$1" = "-o" ]; then
+  echo "run in optimized and half precision"
+  python scripts/webui.py --optimized
+else
+  echo "run in full precision"
+  python scripts/webui.py --no-half --precision=full
+fi
